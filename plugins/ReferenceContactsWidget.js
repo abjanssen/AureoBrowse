d3857e2dc1b60854af721d176d1822bc835f395a
Axel Janssen <janssen.axel@gmail.com>
2025-10-20 09:34:16 +0200

    v0.48

--- plugins/ReferenceContactsWidget.js
+++ plugins/ReferenceContactsWidget.js
@@ -26,10 +26,10 @@ export default class MyPlugin {
       'div', 
       null, 
       React.createElement('h1', null, 'Citing AureoBrowse'),
-      React.createElement('p', null, 'A reference for AureoBrowse will be available soon.'),
-      React.createElement('p', null, ''),
-      React.createElement('p', null, ''),
-      React.createElement("span", null, React.createElement("a", {href: ""}, "" )))
+      React.createElement('p', null, 'Strain-resolved CRISPRi-Seq reveals conserved antibiotic vulnerabilities in ', React.createElement('i', null, 'Staphylococcus aureus'), '.'),
+      React.createElement('p', null, 'Maria-Vittoria Mazzuoli, Jessica Burnier, Albane Schmid, Louise Martin, Axel B. Janssen, Clement Gallay, Annelies S Zinkernagel, Jan-Willem Veening'),
+      React.createElement('p', null, 'bioRxiv, 2025, 2025.10.13.682098'),
+      React.createElement("span", null, React.createElement("a", {href: "https://doi.org/10.1101/2025.10.13.682098", target: "_blank", rel: "noopener noreferrer"}, "https://doi.org/10.1101/2025.10.13.682098" )))
       return React.createElement('div', null, [citecontent])
     }
     const ContactWidget = props => {
